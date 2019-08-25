ce51078b9c24090145f9adb957bd2fd8da9d1340
Ali Gholami <gholamia90@gmail.com>
2019-08-25 21:49:37 +0200

    New: async downloader

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+var outputDir = ""
+var wg sync.WaitGroup
+
 
 // check if given string is an image and should be downloaded
 func okToDownload(s string, hr bool) bool {
@@ -47,42 +53,58 @@ func DownloadFile(url string, filepath string) error {
     return err
 }
 
-func main() {
-	var outputDir = flag.String("output", "/tmp", "output dir to save photos on")
-	var missionID = flag.Int("mission", 11, "number of apollo mission" )
-	var onlyHR = flag.Bool("hr", true, "download only high res photos")
+func worker(queue chan string, worknumber int, mID string) {
+	for link := range queue {
+		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%s/", mID) + link
+		dest := path.Join(outputDir, link)
+		log.Printf("[worker %d] downloading  %s to %s \n", worknumber, imageLink, dest)
+		if err := DownloadFile(imageLink, dest); err != nil {
+			panic(err)
+		}
+		wg.Done()
+	}
+}
 
+func main() {
+	var output = flag.String("output", "/tmp", "output outputDir to save photos on")
+	var mission = flag.Int("mission", 11, "number of apollo mission" )
+	var onlyHR = flag.Bool("hr", true, "download only high res photos")
 	flag.Parse()
 
-	if _, err := os.Stat(*outputDir); os.IsNotExist(err) {
-		log.Fatalf("dir %s does not exist", *outputDir)
+	outputDir = *output
+	missionID := strconv.Itoa(*mission)
+
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		log.Fatalf("outputDir %s does not exist", outputDir)
 	}
 
-	missionIDstr := strconv.Itoa(*missionID)
-	url := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%[1]s/images%[1]s.html", missionIDstr)
+	// queue of jobs
+	q := make(chan string)
+	// init workers
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go worker(q, i, missionID)
+	}
 
+	// init colly
 	c := colly.NewCollector()
-	nDone := 0
-
-
+	// root url to be visited
+	url := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%[1]s/images%[1]s.html", missionID)
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%s/", missionIDstr) + link
+		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%s/", missionID) + link
 		if okToDownload(imageLink, *onlyHR) {
-				dest := path.Join(*outputDir, link)
-				log.Printf("downloading  %s to %s \n", imageLink, dest)
-				if err := DownloadFile(imageLink, dest); err != nil {
-					panic(err)
-    			}
-
+			// send link to job queue
+			wg.Add(1)
+			q <- link
 		}
 	})
-
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
-	fmt.Printf("downloaded %d to %s", nDone, *outputDir)
 
-	c.Visit(url)
+	if err := c.Visit(url); err != nil {
+		log.Fatalf("error happened on visting root url %s %v+\n", url, err)
+	}
+	wg.Wait()
 }
