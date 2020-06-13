09c59e36f6ddcdff8f8944c80b9042346c60e619
Ali Gholami <gholamia90@gmail.com>
2020-06-13 12:28:09 +0200

    Be nice to server using sleeps, and ensure output exists

--- README.md
+++ README.md
@@ -1,28 +1,38 @@
 ## apollo image downloader
 
-This package scrape NASA website to download images of Apollo missions.
+CLI to scrape NASA website to download images of Apollo missions.
 
 ## Build
+
+Make sure you have go installed (tested with go version `1.14`).
+
 ```
 git clone https://gitlab.com/goolila/apollo-images
 cd apollo-images
-GO111MODULE=on go build
+go get -v -t -d ./...
+go build
 ```
-
 
 ## Usage
 ```
 Usage of ./apollo-images:
   -hr
-    	download only high res photos (default true)
+        download only high res photos (default true)
   -mission int
-    	number of apollo mission (default 11)
+        number of apollo mission (default 11)
   -output string
-    	output dir to save photos on (default "/tmp")
+        output outputDir to save photos on (default "/tmp/apollo-images")
+  -sleep int
+        ms to sleep before queueing new url (default 250)
 ```
 
 Example:
 ```
-mkdir -p /tmp/apollo11
-./apollo-images -mission 11 -output /tmp/apollo11
+$ ./apollo-images -mission 12 -sleep 500
+🚀 It was so much fun: https://en.wikipedia.org/wiki/Apollo_11Visiting https://www.hq.nasa.gov/alsj/a11/images11.html
+[worker 1] downloading  https://www.hq.nasa.gov/alsj/a11/a11LunOrb5076HR.jpg to /tmp/apollo-images/11/a11LunOrb5076HR.jpg
+[worker 2] downloading  https://www.hq.nasa.gov/alsj/a11/a11pan1040226combHR.jpg to /tmp/apollo-images/11/a11pan1040226combHR.jpg
+[worker 3] downloading  https://www.hq.nasa.gov/alsj/a11/a11pan1040226lftHR.jpg to /tmp/apollo-images/11/a11pan1040226lftHR.jpg
+[worker 0] downloading  https://www.hq.nasa.gov/alsj/a11/a11pan1040226rghtHR.jpg to /tmp/apollo-images/11/a11pan1040226rghtHR.jpg
+[worker 3] downloading  https://www.hq.nasa.gov/alsj/a11/a11pan1093226HR.jpg to /tmp/apollo-images/11/a11pan1093226HR.jpg
 ```
--- main.go
+++ main.go
@@ -13,11 +13,17 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
-var outputDir = ""
-var wg sync.WaitGroup
-
+var (
+	wg sync.WaitGroup
+	// flags
+	outputDir string
+	missionId int
+	sleep     int
+	onlyHR    bool
+)
 
 // check if given string is an image and should be downloaded
 func okToDownload(s string, hr bool) bool {
@@ -27,87 +33,102 @@ func okToDownload(s string, hr bool) bool {
 	if strings.Contains(s, "..") == true {
 		return false
 	}
-	if hr && strings.Contains(s, "HR") == false{
+	if hr && strings.Contains(s, "HR") == false {
 		return false
 	}
 	return true
 }
 
-
 func DownloadFile(url string, filepath string) error {
-    resp, err := http.Get(url)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    // Create the file
-    out, err := os.Create(filepath)
-    if err != nil {
-        return err
-    }
-    defer out.Close()
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 
-    // Write the body to file
-    _, err = io.Copy(out, resp.Body)
-    return err
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
 
-func worker(queue chan string, worknumber int, mID string) {
+func worker(queue chan string, id int, mID int) {
 	for link := range queue {
-		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%s/", mID) + link
-		dest := path.Join(outputDir, link)
-		log.Printf("[worker %d] downloading  %s to %s \n", worknumber, imageLink, dest)
+		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%d/", mID) + link
+		dest := path.Join(missionDir(mID), link)
+		fmt.Printf("[worker %d] downloading  %s to %s \n", id, imageLink, dest)
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
 		if err := DownloadFile(imageLink, dest); err != nil {
-			panic(err)
+			log.Fatalf("error happened on downloading %s. %+v", imageLink, err)
 		}
 		wg.Done()
 	}
 }
 
 func validateMission(mission int) {
-    switch m := mission; {
+	switch m := mission; {
 	case m < 11:
 		log.Fatalf("Apollo 11 was the spaceflight that first landed humans on the Moon")
 	case m > 17:
 		log.Fatalf("Apollo 17 (December 7–19, 1972) was the final mission of NASA's Apollo program.\n")
 	default:
-		log.Printf("It was so much fun: https://en.wikipedia.org/wiki/Apollo_%d", mission)
-    }
+		fmt.Printf("🚀 It was so much fun: https://en.wikipedia.org/wiki/Apollo_%d", mission)
+	}
+}
+
+func ensureDir(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		log.Printf("outputDir %s does not exist. it will be created.", dir)
+		err = os.MkdirAll(dir, 755)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func missionDir(id int) string {
+	return path.Join(outputDir, strconv.Itoa(id))
 }
 
 func main() {
-	var output = flag.String("output", "/tmp", "output outputDir to save photos on")
-	var mission = flag.Int("mission", 11, "number of apollo mission" )
-	var onlyHR = flag.Bool("hr", true, "download only high res photos")
+	flag.StringVar(&outputDir, "output", "/tmp/apollo-images", "output outputDir to save photos on")
+	flag.IntVar(&missionId, "mission", 11, "number of apollo mission")
+	flag.BoolVar(&onlyHR, "hr", true, "download only high res photos")
+	flag.IntVar(&sleep, "sleep", 250, "ms to sleep before queueing new url")
 	flag.Parse()
 
-	validateMission(*mission)
+	validateMission(missionId)
 
-	outputDir = *output
-	missionID := strconv.Itoa(*mission)
-
-	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
-		log.Fatalf("outputDir %s does not exist", outputDir)
+	mDir := missionDir(missionId)
+	if err := ensureDir(mDir); err != nil {
+		log.Fatalf("output dir %s does not exists and could not be created: %s", mDir, err)
 	}
 
 	// queue of jobs
 	q := make(chan string)
 	// init workers
 	for i := 0; i < runtime.NumCPU(); i++ {
-		go worker(q, i, missionID)
+		go worker(q, i, missionId)
 	}
 
 	// init colly
 	c := colly.NewCollector()
 	// root url to be visited
-	url := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%[1]s/images%[1]s.html", missionID)
+	url := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%[1]d/images%[1]d.html", missionId)
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%s/", missionID) + link
-		if okToDownload(imageLink, *onlyHR) {
+		imageLink := fmt.Sprintf("https://www.hq.nasa.gov/alsj/a%d/", missionId) + link
+		if okToDownload(imageLink, onlyHR) {
 			// send link to job queue
+			time.Sleep(time.Duration(sleep) * time.Millisecond)
 			wg.Add(1)
 			q <- link
 		}
